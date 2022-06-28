2d2b04805ffb53b563f23d3bcbdc350772011a49
Andrey <andrey.y.n.1988@gmail.com>
2022-06-28 23:46:25 +0300

    solution

--- cache.go
+++ cache.go
@@ -1,23 +1,79 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Items struct {
+	deadline         time.Time
+	value            string
+	infinityDeadline bool
+}
 
 type Cache struct {
+	item map[string]Items
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{item: make(map[string]Items)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	var value string
+	// Проверяем наличиие ключа
+	items, exists := c.item[key]
+	if exists {
+		// Проверяем deadline
+		infinityDeadline := items.infinityDeadline
+		if infinityDeadline {
+			// Если дедлайн не ограничен
+			value = items.value
+			return value, exists
+		} else {
+			// Если дедлайн ограничен
+			deadline := items.deadline
+			timeNow := time.Now()
+			elapsed := deadline.Sub(timeNow).Microseconds()
+			if elapsed > 0 {
+				// deadline ещё не достугнут
+				value = items.value
+				return value, exists
+			} else {
+				// deadline превышен
+				return value, false
+			}
+		}
+	} else {
+		// Ключ не найден
+		return value, exists
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	deadline := time.Now()
+	c.item[key] = Items{
+		deadline:         deadline,
+		value:            value,
+		infinityDeadline: true,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var values []string
+	for key := range c.item {
+		// Проверяем на deadline
+		_, exists := c.Get(key)
+		if exists {
+			values = append(values, key)
+		}
+	}
+	return values
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.item[key] = Items{
+		deadline:         deadline,
+		value:            value,
+		infinityDeadline: false,
+	}
 }
